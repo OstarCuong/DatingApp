f444cc9ea7a67a8138405451466acd6eeefd86e5
OstarCuong <OstarCuong@example.com>
2023-07-10 14:52:25 +0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,40 @@
+using System.Text;
 using API.Data;
+
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
+builder.Services.AddCors();
+builder.Services.AddDbContext<DataContext>(otp =>
+                    {
+                         otp.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+                    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
